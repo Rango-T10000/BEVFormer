764797e9f9b2ad7a22d7190ab1a0aceb79e068c9
Rango-T10000 <1335389693wang@gmail.com>
2024-08-08 15:10:18 +0800

    我加入了一些中文的注释，方便我来读懂这个代码

--- tools/test.py
+++ tools/test.py
@@ -24,13 +24,13 @@ from mmdet.datasets import replace_ImageToTensor
 import time
 import os.path as osp
 
-
+#--------解析函数定义，用于解析命令行参数------------
 def parse_args():
     parser = argparse.ArgumentParser(
         description='MMDet test (and eval) a model')
     parser.add_argument('config', help='test config file path')
     parser.add_argument('checkpoint', help='checkpoint file')
-    parser.add_argument('--out', help='output result file in pickle format')
+    parser.add_argument('--out', help='output result file in pickle format')  #参数前面有标记--代表可填可不填
     parser.add_argument(
         '--fuse-conv-bn',
         action='store_true',
@@ -93,7 +93,11 @@ def parse_args():
         default='none',
         help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
+
+    #------------------用parser.parse_args()解析命令行参数，并将结果存储在args中-------------
     args = parser.parse_args()
+
+    #------------------设置环境变量：如果os.environ中不存在LOCAL_RANK，则将args.local_rank值赋给LOCAL_RANK环境变量-----
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
 
@@ -108,28 +112,34 @@ def parse_args():
 
 
 def main():
+    #--------解析函数定义，用于解析命令行参数------------
     args = parse_args()
 
+    #--------assert语句，至少要指定一个操作（保存/评估/仅格式化/显示结果/保存结果----
+    # 通过参数 --out, --eval, --format-only, --show 或 --show-dir（dist_test.sh文件中写了）
     assert args.out or args.eval or args.format_only or args.show \
         or args.show_dir, \
         ('Please specify at least one operation (save/eval/format/show the '
          'results / save the results) with the argument "--out", "--eval"'
          ', "--format-only", "--show" or "--show-dir"')
-
+    #--------异常情况处理----------------------------
     if args.eval and args.format_only:
         raise ValueError('--eval and --format_only cannot be both specified')
-
     if args.out is not None and not args.out.endswith(('.pkl', '.pickle')):
         raise ValueError('The output file must be a pkl file.')
 
+    #--------加载配置文件---------
     cfg = Config.fromfile(args.config)
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
+    
+    #--------导入自定义模块---------
     # import modules from string list.
     if cfg.get('custom_imports', None):
         from mmcv.utils import import_modules_from_strings
         import_modules_from_strings(**cfg['custom_imports'])
 
+    #--------导入插件模块-----------
     # import modules from plguin/xx, registry will be updated
     if hasattr(cfg, 'plugin'):
         if cfg.plugin:
@@ -154,6 +164,7 @@ def main():
                 print(_module_path)
                 plg_lib = importlib.import_module(_module_path)
 
+    #--------设置GPU相关参数--------
     # set cudnn_benchmark
     if cfg.get('cudnn_benchmark', False):
         torch.backends.cudnn.benchmark = True
@@ -162,7 +173,10 @@ def main():
         torch.backends.cuda.matmul.allow_tf32 = False
         torch.backends.cudnn.allow_tf32 = False
 
+    #--------模型在测试阶段不会使用预训练的权重-----
     cfg.model.pretrained = None
+
+    #--------处理测试数据集在连接(concatenated)的情况下的配置--
     # in case the test dataset is concatenated
     samples_per_gpu = 1
     if isinstance(cfg.data.test, dict):
@@ -181,6 +195,7 @@ def main():
             for ds_cfg in cfg.data.test:
                 ds_cfg.pipeline = replace_ImageToTensor(ds_cfg.pipeline)
 
+    #--------初始化分布式环境------------------
     # init distributed env first, since logger depends on the dist info.
     if args.launcher == 'none':
         distributed = False
@@ -188,32 +203,45 @@ def main():
         distributed = True
         init_dist(args.launcher, **cfg.dist_params)
 
+    #--------设置随机种子---------------------
     # set random seeds
     if args.seed is not None:
         set_random_seed(args.seed, deterministic=args.deterministic)
 
-    # build the dataloader
+
+    #--------build the dataloader-----------
     dataset = build_dataset(cfg.data.test)
     data_loader = build_dataloader(
         dataset,
         samples_per_gpu=samples_per_gpu,
         workers_per_gpu=cfg.data.workers_per_gpu,
-        dist=distributed,
+        dist=distributed, 
         shuffle=False,
         nonshuffler_sampler=cfg.data.nonshuffler_sampler,
     )
 
-    # build the model and load checkpoint
-    cfg.model.train_cfg = None
-    model = build_model(cfg.model, test_cfg=cfg.get('test_cfg'))
+ 
+    #----------------------------build the model and load checkpoint------------------------
+    cfg.model.train_cfg = None #表明在测试阶段不会使用任何训练配置
+    
+    #--------构建模型----------
+    model = build_model(cfg.model, test_cfg=cfg.get('test_cfg')) #使用配置文件中的 cfg.model，以及使用测试对应的配置信息
+    
+    #--------若配置文件中存在 fp16 配置，将其提取为 fp16_cfg--------
     fp16_cfg = cfg.get('fp16', None)
     if fp16_cfg is not None:
         wrap_fp16_model(model)
+    
+    #--------load checkpoint，根据配置信息加载模型参数-------------
     checkpoint = load_checkpoint(model, args.checkpoint, map_location='cpu')
+
+    #--------若命令行参数 args.fuse_conv_bn 为真，对模型进行卷积和批归一化的融合--
     if args.fuse_conv_bn:
         model = fuse_conv_bn(model)
+    
     # old versions did not save class info in checkpoints, this walkaround is
     # for backward compatibility
+    #--------处理模型类别信息和调色板信息----------
     if 'CLASSES' in checkpoint.get('meta', {}):
         model.CLASSES = checkpoint['meta']['CLASSES']
     else:
@@ -225,20 +253,22 @@ def main():
         # segmentation dataset has `PALETTE` attribute
         model.PALETTE = dataset.PALETTE
 
+    #--------根据是否在分布式环境下运行，选择了不同的模型并行化策略；这里才是运行model------
     if not distributed:
-        assert False
-        # model = MMDataParallel(model, device_ids=[0])
-        # outputs = single_gpu_test(model, data_loader, args.show, args.show_dir)
+        # assert False
+        model = MMDataParallel(model, device_ids=[0])
+        outputs = single_gpu_test(model, data_loader, args.show, args.show_dir)
     else:
         model = MMDistributedDataParallel(
             model.cuda(),
             device_ids=[torch.cuda.current_device()],
             broadcast_buffers=False)
-        outputs = custom_multi_gpu_test(model, data_loader, args.tmpdir,
-                                        args.gpu_collect)
+        outputs = custom_multi_gpu_test(model, data_loader, args.tmpdir, args.gpu_collect)
 
-    rank, _ = get_dist_info()
-    if rank == 0:
+    #--------基于当前进程的 rank 值，对主进程（rank == 0）进行特定操作---------
+    rank, _ = get_dist_info() #获取分布式信息
+    if rank == 0:             #rank 为 0，表示当前进程是主进程
+        #----若参数配置为--out----
         if args.out:
             print(f'\nwriting results to {args.out}')
             assert False
@@ -246,19 +276,23 @@ def main():
         kwargs = {} if args.eval_options is None else args.eval_options
         kwargs['jsonfile_prefix'] = osp.join('test', args.config.split(
             '/')[-1].split('.')[-2], time.ctime().replace(' ', '_').replace(':', '_'))
+        
+        #----若参数配置为--format_only----
         if args.format_only:
             dataset.format_results(outputs, **kwargs)
-
+        
+        #----若参数配置为--eval----
         if args.eval:
-            eval_kwargs = cfg.get('evaluation', {}).copy()
+            eval_kwargs = cfg.get('evaluation', {}).copy() #复制配置中的评估参数到 eval_kwargs 中
             # hard-code way to remove EvalHook args
             for key in [
                     'interval', 'tmpdir', 'start', 'gpu_collect', 'save_best',
                     'rule'
             ]:
-                eval_kwargs.pop(key, None)
+                eval_kwargs.pop(key, None) #从 eval_kwargs 中移除这些键对应的值
+            #------更新评估参数-------
             eval_kwargs.update(dict(metric=args.eval, **kwargs))
-
+            #------执行评估操作,打印结果----
             print(dataset.evaluate(outputs, **eval_kwargs))
 
 
--- tools/train.py
+++ tools/train.py
@@ -29,13 +29,14 @@ from mmseg import __version__ as mmseg_version
 
 from mmcv.utils import TORCH_VERSION, digit_version
 
-
+#--------解析函数定义，用于解析命令行参数------------
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a detector')
+    parser = argparse.ArgumentParser(description='Train a detector') #创建了一个参数解析器
+
+    #----------使用parser.add_argument()方法添加了多个命令行参数-------------
     parser.add_argument('config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
-    parser.add_argument(
-        '--resume-from', help='the checkpoint file to resume from')
+    parser.add_argument('--resume-from', help='the checkpoint file to resume from')
     parser.add_argument(
         '--no-validate',
         action='store_true',
@@ -84,7 +85,10 @@ def parse_args():
         '--autoscale-lr',
         action='store_true',
         help='automatically scale lr with the number of gpus')
+    #------------------用parser.parse_args()解析命令行参数，并将结果存储在args中-------------
     args = parser.parse_args()
+
+    #------------------设置环境变量：如果os.environ中不存在LOCAL_RANK，则将args.local_rank值赋给LOCAL_RANK环境变量-----
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
 
@@ -100,16 +104,23 @@ def parse_args():
 
 
 def main():
+    #--------解析函数------------
+    #被用来解析用户在命令行中输入的参数，在运行dist_train.sh脚本是命令中传入的参数
+    #调用 parse_args() 函数，将返回的解析结果存储在 args 变量中
     args = parse_args()
 
+    #--------加载配置文件---------
     cfg = Config.fromfile(args.config)
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
+    
+    #--------导入自定义模块---------
     # import modules from string list.
     if cfg.get('custom_imports', None):
         from mmcv.utils import import_modules_from_strings
         import_modules_from_strings(**cfg['custom_imports'])
 
+    #--------导入插件模块-----------
     # import modules from plguin/xx, registry will be updated
     if hasattr(cfg, 'plugin'):
         if cfg.plugin:
@@ -135,6 +146,8 @@ def main():
                 plg_lib = importlib.import_module(_module_path)
 
             from projects.mmdet3d_plugin.bevformer.apis.train import custom_train_model
+    
+    #--------设置GPU相关参数--------
     # set cudnn_benchmark
     if cfg.get('cudnn_benchmark', False):
         torch.backends.cudnn.benchmark = True
@@ -143,6 +156,7 @@ def main():
         torch.backends.cuda.matmul.allow_tf32 = False
         torch.backends.cudnn.allow_tf32 = False
 
+    #--------设置工作目录------------
     # work_dir is determined in this priority: CLI > segment in file > filename
     if args.work_dir is not None:
         # update configs according to CLI args if args.work_dir is not None
@@ -151,19 +165,30 @@ def main():
         # use config filename as default work_dir if cfg.work_dir is None
         cfg.work_dir = osp.join('./work_dirs',
                                 osp.splitext(osp.basename(args.config))[0])
+    
+    #--------如果指定了resume_from参数并且文件存在，则从指定的检查点文件中恢复训练状态--------
     # if args.resume_from is not None:
     if args.resume_from is not None and osp.isfile(args.resume_from):
         cfg.resume_from = args.resume_from
+    
+    #--------GPU IDs设置：用于指定程序在哪些GPU上运行-----------------------------------
     if args.gpu_ids is not None:
         cfg.gpu_ids = args.gpu_ids
     else:
         cfg.gpu_ids = range(1) if args.gpus is None else range(args.gpus)
+    
+    #--------PyTorch版本和优化器类型检查--
+    #--------检查当前使用的PyTorch版本是否为1.8.1，并且检查配置文件中的优化器类型是否为'AdamW'-
     if digit_version(TORCH_VERSION) == digit_version('1.8.1') and cfg.optimizer['type'] == 'AdamW':
         cfg.optimizer['type'] = 'AdamW2' # fix bug in Adamw
+    
+    #--------自动调整学习率--------------
     if args.autoscale_lr:
         # apply the linear scaling rule (https://arxiv.org/abs/1706.02677)
+        #设置为当前学习率乘以GPU数量除以8的结果。这种操作可以用于在分布式训练环境中自动调整学习率，以保持训练的稳定性和效率
         cfg.optimizer['lr'] = cfg.optimizer['lr'] * len(cfg.gpu_ids) / 8
 
+    #--------初始化分布式环境------------
     # init distributed env first, since logger depends on the dist info.
     if args.launcher == 'none':
         distributed = False
@@ -174,11 +199,13 @@ def main():
         _, world_size = get_dist_info()
         cfg.gpu_ids = range(world_size)
 
-    # create work_dir
+    #--------create work_dir：用来保存日志和模型的---------
     mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
-    # dump config
+
+    #--------dump config：将配置文件保存在工作目录中，文件名与命令行参数中指定的配置文件名相同--
     cfg.dump(osp.join(cfg.work_dir, osp.basename(args.config)))
-    # init the logger before other steps
+
+    #--------init the logger before other steps：初始化日志记录器：---------------------
     timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
     log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
     # specify logger name, if we still use 'mmdet', the output info will be
@@ -188,42 +215,52 @@ def main():
         logger_name = 'mmseg'
     else:
         logger_name = 'mmdet'
-    logger = get_root_logger(
-        log_file=log_file, log_level=cfg.log_level, name=logger_name)
+    logger = get_root_logger(log_file=log_file, log_level=cfg.log_level, name=logger_name)
 
+    #-------------收集环境信息，并将其记录在日志中：系统信息、CUDA信息等_start--------------
     # init the meta dict to record some important information such as
     # environment info and seed, which will be logged
     meta = dict()
-    # log env info
+
+    #----log env info----------
     env_info_dict = collect_env()
     env_info = '\n'.join([(f'{k}: {v}') for k, v in env_info_dict.items()])
     dash_line = '-' * 60 + '\n'
-    logger.info('Environment info:\n' + dash_line + env_info + '\n' +
-                dash_line)
+    logger.info('Environment info:\n' + dash_line + env_info + '\n' + dash_line)
     meta['env_info'] = env_info
     meta['config'] = cfg.pretty_text
 
-    # log some basic info
+    #----log some basic info----
     logger.info(f'Distributed training: {distributed}')
     logger.info(f'Config:\n{cfg.pretty_text}')
 
-    # set random seeds
+    #----set random seeds-------
     if args.seed is not None:
         logger.info(f'Set random seed to {args.seed}, '
                     f'deterministic: {args.deterministic}')
         set_random_seed(args.seed, deterministic=args.deterministic)
     cfg.seed = args.seed
     meta['seed'] = args.seed
-    meta['exp_name'] = osp.basename(args.config)
 
+    #--将实验名称保存在meta字典的exp_name键中--
+    #实验名称是根据命令行参数中配置文件路径的基本名称（去除路径部分）得到的
+    meta['exp_name'] = osp.basename(args.config)
+    #-------------收集环境信息，并将其记录在日志中：系统信息、CUDA信息等_end--------------
+
+    #-------------模型构建：传入配置文件中指定的模型参数以及训练和测试配置-----------------
     model = build_model(
         cfg.model,
         train_cfg=cfg.get('train_cfg'),
         test_cfg=cfg.get('test_cfg'))
     model.init_weights()
 
+    #-------------在日志中记录模型的详细信息，包括模型的结构和参数信息---------------------
     logger.info(f'Model:\n{model}')
+
+    #-------------构建训练数据集-----------------------------------------------------
     datasets = [build_dataset(cfg.data.train)]
+    
+    #-------------若配置文件中的 workflow 长度是否为2，为模型准备验证数据集----------------
     if len(cfg.workflow) == 2:
         val_dataset = copy.deepcopy(cfg.data.val)
         # in case we use a dataset wrapper
@@ -236,6 +273,8 @@ def main():
         # refer to https://mmdetection3d.readthedocs.io/en/latest/tutorials/customize_runtime.html#customize-workflow  # noqa
         val_dataset.test_mode = False
         datasets.append(build_dataset(val_dataset))
+    
+    #-------------若配置文件中存在 checkpoint_config 的情况下，为模型的检查点配置添加一些元数据信息----
     if cfg.checkpoint_config is not None:
         # save mmdet version, config file content and class names in
         # checkpoints as meta data
@@ -243,12 +282,16 @@ def main():
             mmdet_version=mmdet_version,
             mmseg_version=mmseg_version,
             mmdet3d_version=mmdet3d_version,
-            config=cfg.pretty_text,
-            CLASSES=datasets[0].CLASSES,
-            PALETTE=datasets[0].PALETTE  # for segmentors
+            config=cfg.pretty_text,      #存储了配置文件的内容，通常是格式化后的文本
+            CLASSES=datasets[0].CLASSES, #存储了数据集的类别信息
+            PALETTE=datasets[0].PALETTE  #用于分割器（segmentors）的调色板信息
             if hasattr(datasets[0], 'PALETTE') else None)
-    # add an attribute for visualization convenience
+    
+    #-------------add an attribute for visualization convenience-------------------
     model.CLASSES = datasets[0].CLASSES
+
+    #-------------执行模型的训练过程，使用指定的数据集、配置参数和其他信息来训练模型----------
+    #/BEVFormer/projects/mmdet3d_plugin/bevformer/apis/train.py中定义的函数
     custom_train_model(
         model,
         datasets,
